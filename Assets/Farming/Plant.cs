1bbb421569359f1237575da3be6396d15ebc2b90
Morgan Cabral <morgan.cabral@gmail.com>
2021-10-02 00:03:14 -0700

    First take on farming related entities

---
+++ Assets/Farming/Plant.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    public Sprite seedlingSprite;
+    public Sprite growingSprite;
+    public Sprite fullyGrownSprite;
+    public Sprite ruinedSprite;
+    public Sprite inventoryIcon;
+
+    private double growthProgress = 0.0;
+    public double growthPerSecond = 1.0;
+    public double totalGrowth = 30.0;
+
+    enum State {
+        Seedling,
+        Growing,
+        FullyGrown,
+        Dying,
+        Ruined
+    }
+}
